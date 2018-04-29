b9a207064bd41490b0c7cdbc1ea3c06789d265ae
Luisa <lemme@thoughtworks.com>
2018-04-29 15:21:10 +0800

    [Luisa][0] Initialize project with gradle and spring boot

---
+++ src/main/java/ec/animal/adoption/AnimalAdoptionApplication.java
@@ -0,0 +1,12 @@
+package ec.animal.adoption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalAdoptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalAdoptionApplication.class, args);
+    }
+}
