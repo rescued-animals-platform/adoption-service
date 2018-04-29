8f469cad8a0c2d77f60f112376dc2f9e954d655a
Luisa <lemme@thoughtworks.com>
2018-04-29 16:24:22 +0800

    [Luisa][1] Refactor animal resource unit tests

--- src/test/java/ec/animal/adoption/resources/AnimalResourceTest.java
+++ src/test/java/ec/animal/adoption/resources/AnimalResourceTest.java
@@ -2,45 +2,26 @@ package ec.animal.adoption.resources;
 
 import ec.animal.adoption.domain.Animal;
 import ec.animal.adoption.services.AnimalService;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AnimalResourceTest {
 
-    @Mock
-    private AnimalService animalService;
-
-    @Mock
-    private Animal animal;
-
-    private AnimalResource animalResource;
-
-    @Before
-    public void setUp() {
-        animalResource = new AnimalResource(animalService);
-    }
-
     @Test
     public void shouldCreateANewAnimal() {
-        animalResource.create(animal);
-
-        verify(animalService).create(animal);
-    }
-
-    @Test
-    public void shouldReturnTheCreatedAnimal() {
         Animal expectedAnimal = mock(Animal.class);
+        Animal animal = mock(Animal.class);
+        AnimalService animalService = mock(AnimalService.class);
         when(animalService.create(animal)).thenReturn(expectedAnimal);
+        AnimalResource animalResource = new AnimalResource(animalService);
+
         Animal createdAnimal = animalResource.create(animal);
 
         assertThat(createdAnimal, is(expectedAnimal));
