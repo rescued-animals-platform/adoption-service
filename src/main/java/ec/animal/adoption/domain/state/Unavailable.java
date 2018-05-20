203abf76cdc9f03d2a4d6590a2f7e930b7cc137c
Luisa <lemme@thoughtworks.com>
2018-05-20 09:28:26 -0500

    [Luisa][2] Refactor serialization, deserialization, equals and hashcode in Unavailable

--- src/main/java/ec/animal/adoption/domain/state/Unavailable.java
+++ src/main/java/ec/animal/adoption/domain/state/Unavailable.java
@@ -1,33 +1,30 @@
 package ec.animal.adoption.domain.state;
 
-import java.io.Serializable;
-import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Unavailable extends State implements Serializable {
-    private String notes;
+public class Unavailable extends State {
 
-    public Unavailable() {
-        // Required for serialization
-    }
+    @JsonProperty("notes")
+    private final String notes;
 
-    public Unavailable(String notes) {
+    @JsonCreator
+    public Unavailable(@JsonProperty("notes") String notes) {
         this.notes = notes;
-    }
-
-    public String getNotes() {
-        return notes;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+
         Unavailable that = (Unavailable) o;
-        return Objects.equals(notes, that.notes);
+
+        return notes != null ? notes.equals(that.notes) : that.notes == null;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(notes);
+        return notes != null ? notes.hashCode() : 0;
     }
 }
