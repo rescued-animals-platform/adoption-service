ae6cc0d6a098f816db55eb607498c1cf76ce3ca0
Luisa <lemme@thoughtworks.com>
2018-05-21 12:34:39 -0500

    [Luisa][3] Refactor minor format things

--- src/main/java/ec/animal/adoption/resources/AnimalResource.java
+++ src/main/java/ec/animal/adoption/resources/AnimalResource.java
@@ -15,6 +15,7 @@ import javax.validation.Valid;
 
 @RestController
 public class AnimalResource {
+
     private final AnimalService animalService;
 
     @Autowired
--- src/test/java/ec/animal/adoption/services/AnimalServiceTest.java
+++ src/test/java/ec/animal/adoption/services/AnimalServiceTest.java
@@ -3,35 +3,22 @@ package ec.animal.adoption.services;
 import ec.animal.adoption.domain.Animal;
 import ec.animal.adoption.exceptions.EntityAlreadyExistsException;
 import ec.animal.adoption.repositories.AnimalRepository;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
 public class AnimalServiceTest {
-
-    @Mock
-    private AnimalRepository animalRepository;
-
-    private AnimalService animalService;
-
-    @Before
-    public void setUp() {
-        animalService = new AnimalService(animalRepository);
-    }
 
     @Test
     public void shouldCreateAnAnimal() throws EntityAlreadyExistsException {
         Animal expectedAnimal = mock(Animal.class);
         Animal animal = mock(Animal.class);
+        AnimalRepository animalRepository = mock(AnimalRepository.class);
         when(animalRepository.save(animal)).thenReturn(expectedAnimal);
+        AnimalService animalService = new AnimalService(animalRepository);
 
         Animal createdAnimal = animalService.create(animal);
 
