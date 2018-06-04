de4b8120b04b44c297b10810ff6344692847d6d0
Luisa <lemme@thoughtworks.com>
2018-06-04 16:15:22 -0500

    [Luisa][6] Fix install terraform script

--- scripts/install-terraform.sh
+++ scripts/install-terraform.sh
@@ -7,6 +7,6 @@ wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd6
 
 mkdir -p ~/opt/terraform;
 unzip terraform_0.11.7_linux_amd64.zip -d ~/opt/terraform;
-cp opt/terraform/terraform /usr/local/bin/;
+cp ~/opt/terraform/terraform /usr/local/bin/;
 
 terraform --version
