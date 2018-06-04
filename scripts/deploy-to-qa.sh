884420fa0ff7aad478cbabb20cb597f67b1f1cb3
Luisa <lemme@thoughtworks.com>
2018-06-04 16:41:27 -0500

    [Luisa][6] Simplify deploy to QA job. Add terraform installation and google cloud authentication setup to deploy-to-qa.sh script

--- scripts/deploy-to-qa.sh
+++ scripts/deploy-to-qa.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-cd terraform;
+echo "Installing Terraform v0.11.7"
 
+apt-get update;
+apt-get install -y unzip wget;
+wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip;
+
+mkdir -p ~/opt/terraform;
+unzip terraform_0.11.7_linux_amd64.zip -d ~/opt/terraform;
+cp ~/opt/terraform/terraform /usr/local/bin/;
+
+terraform --version
+
+echo "Setting gcloud authentication"
+
+echo $GCLOUD_SERVICE_KEY > ${HOME}/gcloud-service-key.json;
+gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json;
+gcloud config set project $GCLOUD_PROJECT_ID;
+export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcloud-service-key.json
+
+echo "Deploying application"
+
+cd terraform;
 terraform init;
 terraform apply -auto-approve;
-
 export CONNECTION_NAME=$(terraform output db_connection_name)
-
 ./gradlew appengineDeploy
