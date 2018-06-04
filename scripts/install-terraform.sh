8a571697b5d1dda153e01d235a04fa71b7a105ef
Luisa <lemme@thoughtworks.com>
2018-06-04 15:58:05 -0500

    [Luisa][6] Fixing sudo command not found

--- scripts/install-terraform.sh
+++ scripts/install-terraform.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-sudo apt-get update;
-sudo apt-get install -y unzip;
+ls /usr/bin
+
+/usr/bin/sudo apt-get update;
+/usr/bin/sudo apt-get install -y unzip;
 wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip;
 
 mkdir -p ~/opt/terraform;
 unzip terraform_0.11.1_linux_amd64.zip -d ~/opt/terraform;
-sudo cp opt/terraform/terraform /usr/local/bin/;
+/usr/bin/sudo cp opt/terraform/terraform /usr/local/bin/;
 
 terraform --version
