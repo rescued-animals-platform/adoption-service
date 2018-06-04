723c097c3e2ca6256a4ed1264b6f1db6a09f268e
Luisa <lemme@thoughtworks.com>
2018-06-04 15:51:32 -0500

    [Luisa][6] Extracts terraform installation and gcloud authentication to scripts

---
+++ scripts/authenticate-gcloud.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+echo $GCLOUD_SERVICE_KEY > ${HOME}/gcloud-service-key.json;
+
+sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json;
+
+sudo /opt/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT_ID;
