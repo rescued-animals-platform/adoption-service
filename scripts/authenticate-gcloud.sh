9267f0e6ef8d1cfb94d6fcbcef00d713f636f3b4
Luisa <lemme@thoughtworks.com>
2018-06-04 16:07:01 -0500

    [Luisa][6] Install wget

--- scripts/authenticate-gcloud.sh
+++ scripts/authenticate-gcloud.sh
@@ -3,6 +3,6 @@ set -xeuo pipefail
 
 echo $GCLOUD_SERVICE_KEY > ${HOME}/gcloud-service-key.json;
 
-sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json;
+/opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json;
 
-sudo /opt/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT_ID;
+/opt/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT_ID;
--- scripts/install-terraform.sh
+++ scripts/install-terraform.sh
@@ -2,7 +2,7 @@
 set -xeuo pipefail
 
 apt-get update;
-apt-get install -y unzip;
+apt-get install -y unzip wget;
 wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip;
 
 mkdir -p ~/opt/terraform;
