44f57ed0ebad388830fb7412b971dd9d09976a0a
Luisa <lemme@thoughtworks.com>
2018-06-04 16:02:37 -0500

    [Luisa][6] Removing sudo

--- scripts/install-terraform.sh
+++ scripts/install-terraform.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-ls /usr/bin
-
-/usr/bin/sudo apt-get update;
-/usr/bin/sudo apt-get install -y unzip;
+apt-get update;
+apt-get install -y unzip;
 wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip;
 
 mkdir -p ~/opt/terraform;
 unzip terraform_0.11.1_linux_amd64.zip -d ~/opt/terraform;
-/usr/bin/sudo cp opt/terraform/terraform /usr/local/bin/;
+cp opt/terraform/terraform /usr/local/bin/;
 
 terraform --version
