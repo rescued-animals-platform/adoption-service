41df087abd43165b866e2a1dd70d69d33e895c62
Luisa <lemme@thoughtworks.com>
2018-06-05 16:55:44 -0500

    [Luisa][6] Auto approve oracle license

--- scripts/deploy-to-qa.sh
+++ scripts/deploy-to-qa.sh
@@ -18,6 +18,8 @@ echo "Installing Oracle JDK 8"
 apt-get install -y software-properties-common
 add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main"
 apt-get update
+echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
 apt-get install -y oracle-java8-installer
 java -version
 
