4ce85dbd6e00b1930f91b3f27bbcab50324cffe1
Luisa <lemme@thoughtworks.com>
2019-07-01 13:58:15 -0500

    [Luisa][13] Arranges connections and env variables to run integration tests in ci

--- scripts/ci/ci-storages-deploy.sh
+++ scripts/ci/ci-storages-deploy.sh
@@ -18,12 +18,4 @@ echo "Deploying Storages in CI project"
 cd terraform;
 terraform init;
 terraform apply -auto-approve -var-file="ci.tfvars";
-export DB_CONNECTION_NAME=$(terraform output master_proxy_connection)
-
-{
-  APP_YAML_DIR=~/repo/src/main/appengine
-  sed "s/DB_CONNECTION_STRING/${DB_CONNECTION_NAME}/g" ${APP_YAML_DIR}/app.yaml.template >> ${APP_YAML_DIR}/app.yaml.stashed
-  sed "s/DB_PASSWORD/${CI_SQL_CLOUD_MASTER_PASSWORD}/g" ${APP_YAML_DIR}/app.yaml.stashed >> ${APP_YAML_DIR}/app.yaml.stashed
-  sed "s/DB_USERNAME/${CI_SQL_CLOUD_MASTER_USERNAME}/g" ${APP_YAML_DIR}/app.yaml.stashed >> ${APP_YAML_DIR}/app.yaml
-  rm ${APP_YAML_DIR}/app.yaml.stashed
-} &> /dev/null
+echo "export DB_CONNECTION_NAME=$(terraform output master_proxy_connection)" >> ${HOME}/terraform/db.env
