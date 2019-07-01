2dec2052822ac7e767ae7cedc33c1d4c9d6f4df9
Luisa <lemme@thoughtworks.com>
2019-07-01 15:10:58 -0500

    [Luisa][13] Adds a script for integration tests in ci

--- scripts/ci/ci-integration-test.sh
+++ scripts/ci/ci-integration-test.sh
@@ -7,3 +7,8 @@ echo "Gcloud auth set up"
   echo $GCLOUD_CI_SERVICE_KEY >> ${HOME}/gcloud-service-key.json;
   export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcloud-service-key.json
 } &> /dev/null
+
+echo "Running Integration Tests"
+
+source ${HOME}/repo/db.env
+./gradlew integrationTest
--- scripts/ci/ci-storages-deploy.sh
+++ scripts/ci/ci-storages-deploy.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
+echo "Gcloud auth set up"
+
+{
+  echo $GCLOUD_CI_SERVICE_KEY >> ${HOME}/gcloud-service-key.json;
+  export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcloud-service-key.json
+} &> /dev/null
+
 echo "Deploying Storages in CI project"
 
 {
