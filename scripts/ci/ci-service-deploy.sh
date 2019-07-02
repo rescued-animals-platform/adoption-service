293c98efc50a9e8c25d652c4ea8fe9fc4f3c5a57
Luisa <lemme@thoughtworks.com>
2019-07-02 11:50:26 -0500

    [Luisa][13] Fixes script to deploy service

--- scripts/ci/ci-service-deploy.sh
+++ scripts/ci/ci-service-deploy.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-cd ~/repo
-
 echo "Gcloud auth set up"
 
 {
@@ -10,22 +8,20 @@ echo "Gcloud auth set up"
   gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json;
 } &> /dev/null
 
-echo "Setting active spring profile in app.yaml"
+echo "Setting variable values in app.yaml"
 
-APP_YAML_DIR=~/repo/src/main/appengine
-sed "s/SPRING_PROFILE/${SPRING_PROFILE}/g" ${APP_YAML_DIR}/app.yaml.template > ${APP_YAML_DIR}/app.yaml
-
-echo "Deploying application"
-
-cat ~/.profile
-
-source ${HOME}/repo/db.env >> ~/.bashrc
-source ${HOME}/repo/db.env >> ~/.profile
-
-echo "DB_CONNECTION_NAME=${DB_CONNECTION_NAME}" >> /etc/environment
-
-hash -r
+source ${HOME}/repo/db.env
 
 echo $DB_CONNECTION_NAME
 
+APP_YAML_DIR=~/repo/src/main/appengine
+sed "s/SPRING_PROFILE/${SPRING_PROFILE}/g" ${APP_YAML_DIR}/app.yaml.template > ${APP_YAML_DIR}/app.yaml.stashed
+sed "s/DB_CONNECTION_STRING/${DB_CONNECTION_NAME}/g" ${APP_YAML_DIR}/app.yaml.stashed > ${APP_YAML_DIR}/app.yaml
+rm ${APP_YAML_DIR}/app.yaml.stashed
+
+cat ${APP_YAML_DIR}/app.yaml
+
+echo "Deploying application"
+
+cd ~/repo
 ./gradlew deployToAppEngine
