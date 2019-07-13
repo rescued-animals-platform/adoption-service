808d105ba9e31646d7a4dc317f263e5a5a02249b
Luisa <lemme@thoughtworks.com>
2019-07-13 12:39:49 -0500

    [Luisa][4] Troubleshooting service deployment

--- scripts/ci-service-deploy.sh
+++ scripts/ci-service-deploy.sh
@@ -6,18 +6,18 @@ echo "Gcloud auth set up"
 {
   echo $CI_GCLOUD_SERVICE_KEY >> ${HOME}/gcloud-service-key.json;
   export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcloud-service-key.json;
-#  gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS};
+  gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS};
 } &> /dev/null
 
 echo "Kubectl set up"
-
-source ${HOME}/repo/env.properties
-gcloud container clusters get-credentials ${CI_CLUSTER_NAME} --zone=${CI_CLUSTER_ZONE} --project=${CI_CLUSTER_PROJECT}
 
 {
   export TF_VAR_cloud_sql_master_user_name=${CI_SQL_CLOUD_MASTER_USERNAME}
   export TF_VAR_cloud_sql_master_user_password=${CI_SQL_CLOUD_MASTER_PASSWORD}
   echo $CI_ADOPTION_SERVICE_SERVICE_KEY >> ${HOME}/repo/terraform/deployment/credentials.json;
+
+  source ${HOME}/repo/env.properties
+  gcloud container clusters get-credentials ${CI_CLUSTER_NAME} --zone=${CI_CLUSTER_ZONE} --project=${CI_CLUSTER_PROJECT}
 } &> /dev/null
 
 echo "Deploying service dependencies with terraform"
