44bbe4e053dfb7d1864c71f20eeaf7b3a7360aa5
Luisa <lemme@thoughtworks.com>
2019-07-16 18:32:06 -0500

    [Luisa][4] Fixes ci-api-test script

--- scripts/ci-api-test.sh
+++ scripts/ci-api-test.sh
@@ -6,6 +6,7 @@ echo "Gcloud auth set up"
 {
   echo $CI_GCLOUD_SERVICE_KEY >> ${HOME}/gcloud-service-key.json;
   export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcloud-service-key.json
+  export ENVIRONMENT_PROPERTIES_PATH=${HOME}/repo/env.properties
 } &> /dev/null
 
 echo "Preparing environment"
