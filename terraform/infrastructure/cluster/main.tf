05681f668bcc52e4ff147e3e025b90536cc69ed2
Luisa <lemme@thoughtworks.com>
2019-07-23 20:06:09 -0500

    [Luisa][4] Changes oauth scope for storage read/write permissions

--- terraform/infrastructure/cluster/main.tf
+++ terraform/infrastructure/cluster/main.tf
@@ -21,7 +21,7 @@ resource "google_container_cluster" "gcp_kubernetes" {
   node_config {
     machine_type = var.cluster_node_machine_type
     oauth_scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/devstorage.read_write",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
       "https://www.googleapis.com/auth/service.management.readonly",
