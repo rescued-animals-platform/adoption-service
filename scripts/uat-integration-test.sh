f8e04187b32b9baaf763747c643b564ac08bb79d
Luisa Emme <emmeblm@hotmail.com>
2020-04-14 19:44:48 +0200

    [Luisa][34] Separates script

---
+++ scripts/uat-integration-test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+CLOUDINARY_URL="$(heroku config:get CLOUDINARY_URL -a servicio-adopciones-uat)";
+SPRING_DATASOURCE_URL="$(heroku run -a servicio-adopciones-uat echo \$SPRING_DATASOURCE_URL)";
+SPRING_DATASOURCE_USERNAME="$(heroku run -a servicio-adopciones-uat echo \$SPRING_DATASOURCE_USERNAME)";
+SPRING_DATASOURCE_PASSWORD="$(heroku run -a servicio-adopciones-uat echo \$SPRING_DATASOURCE_PASSWORD)";
+
+export CLOUDINARY_URL;
+export SPRING_DATASOURCE_URL;
+export SPRING_DATASOURCE_USERNAME;
+export SPRING_DATASOURCE_PASSWORD;
+export SPRING_PROFILE=live;
+export ADOPTION_SERVICE_URL=https://servicio-adopciones-uat.herokuapp.com;
+
+./gradlew integrationTest --rerun-tasks
