df2c770343ff380dfa31906508354cc25eadae00
Luisa Emme <emmeblm@hotmail.com>
2020-04-14 19:58:19 +0200

    [Luisa][34] Fixes string on database url

--- scripts/uat-integration-test.sh
+++ scripts/uat-integration-test.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-CLOUDINARY_URL="$(heroku config:get CLOUDINARY_URL -a servicio-adopciones-uat)";
-SPRING_DATASOURCE_URL="$(heroku run -a servicio-adopciones-uat echo \$SPRING_DATASOURCE_URL)";
-SPRING_DATASOURCE_USERNAME="$(heroku run -a servicio-adopciones-uat echo \$SPRING_DATASOURCE_USERNAME)";
-SPRING_DATASOURCE_PASSWORD="$(heroku run -a servicio-adopciones-uat echo \$SPRING_DATASOURCE_PASSWORD)";
+echo "Getting settings from heroku"
+
+{
+  CLOUDINARY_URL="$(heroku config:get CLOUDINARY_URL -a servicio-adopciones-uat)";
+  SPRING_DATASOURCE_URL="$(heroku run -a servicio-adopciones-uat echo \$SPRING_DATASOURCE_URL)";
+  SPRING_DATASOURCE_URL_FIXED="${SPRING_DATASOURCE_URL:4}"
+  SPRING_DATASOURCE_USERNAME="$(heroku run -a servicio-adopciones-uat echo \$SPRING_DATASOURCE_USERNAME)";
+  SPRING_DATASOURCE_PASSWORD="$(heroku run -a servicio-adopciones-uat echo \$SPRING_DATASOURCE_PASSWORD)";
+} &> /dev/null
 
 export CLOUDINARY_URL;
-export SPRING_DATASOURCE_URL;
+export SPRING_DATASOURCE_URL_FIXED;
 export SPRING_DATASOURCE_USERNAME;
 export SPRING_DATASOURCE_PASSWORD;
 export SPRING_PROFILE=live;
