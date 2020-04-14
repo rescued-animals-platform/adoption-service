498b16717528e0b97ec7aeab53cc4057bfb6b9eb
Luisa Emme <emmeblm@hotmail.com>
2020-04-14 20:41:40 +0200

    [Luisa][34] Cleans up terraform and adds api test job to circle ci

--- terraform/deployment/outputs.tf
+++
@@ -1,8 +0,0 @@
-# ------------------------------------------------------------------------------
-# MASTER OUTPUTS
-# ------------------------------------------------------------------------------
-
-output "adoption_service_ip" {
-  description = "Endpoint for connections to the deployed adoption service"
-  value = module.service.adoption_service_ip
-}
--- terraform/deployment/service/outputs.tf
+++
@@ -1,7 +0,0 @@
-# ------------------------------------------------------------------------------
-# MASTER OUTPUTS
-# ------------------------------------------------------------------------------
-
-output "adoption_service_ip" {
-  value = kubernetes_service.adoption-service-ingress.load_balancer_ingress[0].ip
-}
