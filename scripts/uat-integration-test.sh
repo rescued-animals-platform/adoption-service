93c58162063609a806f7696d9bb3ad0f27635978
Luisa Emme <emmeblm@hotmail.com>
2020-04-14 20:14:27 +0200

    [Luisa][34] Fixes string on database url

--- scripts/uat-integration-test.sh
+++ scripts/uat-integration-test.sh
@@ -5,14 +5,14 @@ echo "Getting settings from heroku"
 
 {
   CLOUDINARY_URL="$(heroku config:get CLOUDINARY_URL -a servicio-adopciones-uat)";
-  SPRING_DATASOURCE_URL="$(heroku run -a servicio-adopciones-uat echo \$SPRING_DATASOURCE_URL)";
-  SPRING_DATASOURCE_URL_FIXED="${SPRING_DATASOURCE_URL:4}"
+  SPRING_DATASOURCE_URL_TO_FIX="$(heroku run -a servicio-adopciones-uat echo \$SPRING_DATASOURCE_URL)";
+  SPRING_DATASOURCE_URL="${SPRING_DATASOURCE_URL_TO_FIX:4}"
   SPRING_DATASOURCE_USERNAME="$(heroku run -a servicio-adopciones-uat echo \$SPRING_DATASOURCE_USERNAME)";
   SPRING_DATASOURCE_PASSWORD="$(heroku run -a servicio-adopciones-uat echo \$SPRING_DATASOURCE_PASSWORD)";
 } &> /dev/null
 
 export CLOUDINARY_URL;
-export SPRING_DATASOURCE_URL_FIXED;
+export SPRING_DATASOURCE_URL;
 export SPRING_DATASOURCE_USERNAME;
 export SPRING_DATASOURCE_PASSWORD;
 export SPRING_PROFILE=live;
