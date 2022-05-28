2403dae61ab5eba4e2b305cf2a04a783fa8b0b3f
Luisa Emme <emmeblm@users.noreply.github.com>
2022-05-28 19:51:02 +0200

    [Luisa][43] Improves JpaCharacteristics Mapping logic and adds *MapperImpl.java classes back to pitest analysis to cover mapping behaviour

--- src/main/java/ec/animal/adoption/adapter/jpa/service/JpaCharacteristicsMapper.java
+++ src/main/java/ec/animal/adoption/adapter/jpa/service/JpaCharacteristicsMapper.java
@@ -5,6 +5,10 @@ import ec.animal.adoption.adapter.jpa.model.JpaCharacteristics;
 import ec.animal.adoption.adapter.jpa.model.JpaFriendlyWith;
 import ec.animal.adoption.domain.model.characteristics.Characteristics;
 import ec.animal.adoption.domain.model.characteristics.FriendlyWith;
+import ec.animal.adoption.domain.model.characteristics.temperaments.Balance;
+import ec.animal.adoption.domain.model.characteristics.temperaments.Docility;
+import ec.animal.adoption.domain.model.characteristics.temperaments.Sociability;
+import ec.animal.adoption.domain.model.characteristics.temperaments.Temperaments;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
@@ -36,18 +40,36 @@ public interface JpaCharacteristicsMapper {
 
     @Mapping(source = "characteristics.identifier", target = "id", defaultExpression = "java( UUID.randomUUID() )")
     @Mapping(source = "characteristics.registrationDate", target = "registrationDate", defaultExpression = "java( LocalDateTime.now() )")
-    @Mapping(source = "characteristics.temperaments.sociability", target = "sociability", qualifiedByName = "mapEnum")
-    @Mapping(source = "characteristics.temperaments.docility", target = "docility", qualifiedByName = "mapEnum")
-    @Mapping(source = "characteristics.temperaments.balance", target = "balance", qualifiedByName = "mapEnum")
+    @Mapping(source = "characteristics.temperaments", target = "sociability", qualifiedByName = "toStringSociability")
+    @Mapping(source = "characteristics.temperaments", target = "docility", qualifiedByName = "toStringDocility")
+    @Mapping(source = "characteristics.temperaments", target = "balance", qualifiedByName = "toStringBalance")
     JpaCharacteristics toJpaCharacteristics(Characteristics characteristics, JpaAnimal jpaAnimal);
 
     default List<JpaFriendlyWith> toJpaFriendlyWithCollection(Set<FriendlyWith> friendlyWith) {
         return friendlyWith.stream().map(Enum::name).map(JpaFriendlyWith::new).toList();
     }
 
-    @Named("mapEnum")
-    @SuppressWarnings({"OptionalUsedAsFieldOrParameterType", "rawtypes"})
-    default String map(Optional<? extends Enum> anEnum) {
-        return anEnum.map(Enum::name).orElse(null);
+    @Named("toStringSociability")
+    default String toStringSociability(Temperaments temperaments) {
+        return Optional.ofNullable(temperaments)
+                       .flatMap(Temperaments::getSociability)
+                       .map(Sociability::name)
+                       .orElse(null);
+    }
+
+    @Named("toStringDocility")
+    default String toStringDocility(Temperaments temperaments) {
+        return Optional.ofNullable(temperaments)
+                       .flatMap(Temperaments::getDocility)
+                       .map(Docility::name)
+                       .orElse(null);
+    }
+
+    @Named("toStringBalance")
+    default String toStringBalance(Temperaments temperaments) {
+        return Optional.ofNullable(temperaments)
+                       .flatMap(Temperaments::getBalance)
+                       .map(Balance::name)
+                       .orElse(null);
     }
 }
